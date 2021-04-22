bb3c0f1fb5bfa60af9709b1084aafb3710f19dd0
FlaviusGorgan <gorganflavius@gmail.com>
2021-04-22 22:51:06 +0300

    Register form and variable encaps for BE

---
+++ src/app/auth/register-payload.ts
@@ -0,0 +1,6 @@
+export class RegisterPayload {
+    username: String;
+    password: String;
+    email: String;
+    confirmedPassword: String;
+}
---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { RegisterPayload } from '../register-payload';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  // create formGrup
+  registerForm: FormGroup;
+  registerPayload: RegisterPayload;
+
+  // initializam formGrup in componenta Register, injectand in constructor
+  constructor(private formBuilder: FormBuilder) {
+    this.formBuilder.group({
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    });
+
+    this.registerPayload = {
+      username: '',
+      password: '',
+      email: '',
+      confirmedPassword: '',
+    };
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.registerPayload.username = this.registerForm.get('username').value;
+    this.registerPayload.email = this.registerForm.get('email').value;
+    this.registerPayload.password = this.registerForm.get('password').value;
+    this.registerPayload.confirmedPassword = this.registerForm.get('confirmedPassword').value;
+    
+  }
+}

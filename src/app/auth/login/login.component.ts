63c7433daf6c0b21862bb11bfb576a65a644ef8a
FlaviusGorgan <gorganflavius@gmail.com>
2021-04-27 23:10:39 +0300

    JwtAutResponse and NgxWebStorageModule added

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/auth.service';
+import { LoginPayload } from '../login-payload';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loginPayload: LoginPayload;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
+    this.loginForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+    });
   }
 
+  ngOnInit(): void {}
+
+  // submit the form to do the login
+  onSubmit() {
+    //extracted user si pass prin login form; ca sa le trimite trimitem catre BE , trebuie sa la encasulam intr-un obiect si trimis catre authService sa faca call-ul, vom crea clasa LoginPayload, similar cu RegisterPayload
+    this.loginPayload.username = this.loginForm.get('username').value;
+    this.loginPayload.password = this.loginForm.get('password').value;
+
+    this.authService.login(this.loginPayload);
+  }
 }
